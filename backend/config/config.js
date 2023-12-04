052d6c88efbff3f7e2eec3e655cc65c826450984
Jonah Samuel <samueljonas922@gmail.com>
2023-12-04 23:59:21 +1100

    full app

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,14 +4,21 @@ require('dotenv').config();
 module.exports = {
     port: process.env.PORT,
     db: {
-        database: process.env.DD_NAME,
+        host: process.env.HOST,
+        port: process.env.PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
+        database: process.env.DD_NAME,
         options: {
             dialect: process.env.DIALECT,
-            host: process.env.localhost,
-            storage: './data/gragecms.sqlite'
+            ssl: true,
+            dialectOptions: {
+                ssl: {
+                  require: true,
+                },
+            // storage: './data/gragecms.sqlite'
         }
+    }
     },
     auth: {
         jwtSecret: process.env.JWT_SECRET
