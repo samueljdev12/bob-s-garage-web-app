d6ebd78d59abefe7ee2e46128b7af5ac3cac9bbc
Jonah Samuel <samueljonas922@gmail.com>
2023-12-07 16:27:28 +1100

    chnaged

--- frontend/src/components/layouts/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-  return (
-    <div className="alert alert-danger" role="alert" id="show" style={{display: "none"}}>
-    A simple danger alert—check it out!
-  </div>
-  );
-};
-
-export default Error;
--- frontend/src/utils/showError.js
+++
@@ -1,7 +0,0 @@
-export function showContainerError(errorMessage) {
-  var containerError = $('#show'); // Use jQuery selector
-  if (containerError.length) {
-    containerError.text(errorMessage);
-    containerError.fadeIn();
-  }
-}
